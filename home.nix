5bff5ba099184e047b8b62eba503bb855e939b72
Pascal <pascalsolarluchs@gmail.com>
2026-05-06 11:35:11 -0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -5,16 +5,16 @@
     inputs.plasma-manager.homeModules.plasma-manager
   ];
 
-  home.username = "fumo";
-  home.homeDirectory = "/home/fumo";
+  home.username = "pasc";
+  home.homeDirectory = "/home/pasc";
   home.stateVersion = "25.11";
 
   programs.git = {
     enable = true;
     settings = {
       user = {
-        name = "FumoFumoEnjoyer";
-        email = "FumoFumoEnjoyer@fumofumo.dev";
+        name = "Pascal";
+        email = "pascalsolarluchs@gmail.com";
       };
     };
   };
@@ -34,79 +34,6 @@
       enable = true;
       theme = "dpoggi";
       plugins = [ "git" "sudo" ];
-    };
-  };
-
-  programs.plasma = {
-    enable = true;
-    workspace.lookAndFeel = "org.kde.breezedark.desktop";
-    panels = [
-      {
-        location = "bottom";
-        floating = true;
-        height = 32;
-        widgets = [
-          {
-            kickoff = {
-              icon = "nix-snowflake-white";
-              sortAlphabetically = true;
-            };
-          }
-          "org.kde.plasma.icontasks"
-          "org.kde.plasma.marginsseparator"
-          {
-            pager = { };
-          }
-          "org.kde.plasma.systemtray"
-          {
-            digitalClock = {
-              time.format = "24h";
-              calendar.firstDayOfWeek = "monday";
-            };
-          }
-          "org.kde.plasma.showdesktop"
-        ];
-      }
-    ];
-
-
-
-    shortcuts = {
-
-      kwin."Switch to Desktop 1" = "Meta+1";
-      kwin."Switch to Desktop 2" = "Meta+2";
-      kwin."Switch to Desktop 3" = "Meta+3";
-      kwin."Switch to Desktop 4" = "Meta+4";
-      kwin."Switch to Desktop 5" = "Meta+5";
-      kwin."Switch to Desktop 6" = "Meta+6";
-      kwin."Switch to Desktop 7" = "Meta+7";
-      kwin."Switch to Desktop 8" = "Meta+8";
-      kwin."Switch to Desktop 9" = "Meta+9";
-
-      kwin."Window to Desktop 1" = "Meta+!";
-      kwin."Window to Desktop 2" = "Meta+@";
-      kwin."Window to Desktop 3" = "Meta+#";
-      kwin."Window to Desktop 4" = "Meta+$";
-      kwin."Window to Desktop 5" = "Meta+%";
-      kwin."Window to Desktop 6" = "Meta+^";
-      kwin."Window to Desktop 7" = "Meta+&";
-      kwin."Window to Desktop 8" = "Meta+*";
-      kwin."Window to Desktop 9" = "Meta+(";
-
-      "services/brave-browser.desktop"._launch = "Meta+B";
-      "services/org.kde.konsole.desktop"._launch = [ "Meta+Return" "Meta+T" ];
-
-
-    };
-    spectacle.shortcuts = {
-      captureEntireDesktop = "Meta+Print";
-      captureRectangularRegion = "Meta+Shift+Print";
-      captureActiveWindow = "Meta+Ctrl+Print";
-    };
-
-    configFile = {
-      kwinrc.Desktops.Number = 4;
-      kwinrc.Desktops.Rows = 1;
     };
   };
 }
